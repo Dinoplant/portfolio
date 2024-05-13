fadc5a3778af051b0dc636f74afbaf04ca6dc691
Dinoplant <angel.delgado_jimenez23@student.cart.com>
2024-05-12 23:49:50 -0700

    added the image changer and mario images

--- app.js
+++ app.js
@@ -1,3 +1,70 @@
-function changePage(page){
-    location.href=page
+let gDNumber = 1
+let imageNumber = 0
+
+function changePage(page) {
+    location.href = page
+}
+
+function imageChanger() {
+    imageNumber = Math.floor(Math.random() * 10)
+    console.log(codeImageId)
+    console.log(imageNumber)
+    switch (imageNumber) {
+        case 0:
+        case 1:
+        case 2:
+            codeImageId.className = ``
+            codeImageId.classList.add(`codeImages`);
+            codeImageId.classList.add(`js`);
+            break;
+        case 3:
+        case 4:
+        case 5:
+            codeImageId.className = ``
+            codeImageId.classList.add(`codeImages`);
+            codeImageId.classList.add(`html`);
+            break;
+        case 6:
+        case 7:
+        case 8:
+            codeImageId.className = ``
+            codeImageId.classList.add(`codeImages`);
+            codeImageId.classList.add(`css`);
+            break;
+        case 9:
+            codeImageId.className = ``
+            codeImageId.classList.add(`codeImages`);
+            codeImageId.classList.add(`trio`);
+            break;
+        default:
+            console.log(`sorry no work`);
+    }
+}
+
+function gameDesignImages (){
+
+    switch (gDNumber) {
+        case 1:
+            gameDesignExampleId.className = ``
+            gameDesignExampleId.classList.add(`gameDesignExample`);
+            gameDesignExampleId.classList.add(`mario-Blank`);
+            gDNumber++
+            break;
+        case 2:
+            gameDesignExampleId.className = ``
+            gameDesignExampleId.classList.add(`gameDesignExample`);
+            gameDesignExampleId.classList.add(`mario-Block`);
+            gDNumber++
+            break;
+        case 3:
+            gameDesignExampleId.className = ``
+            gameDesignExampleId.classList.add(`gameDesignExample`);
+            gameDesignExampleId.classList.add(`mario-Cliff`);
+            gDNumber = 1
+            break;
+        default:
+            console.log(`sorry no work`);
+    }
+
+
 }
